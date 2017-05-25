8fccd350a8faf9a07db957fa19e798b7a884b99c
J. Manuel Colmenar <josemanuel.colmenar@urjc.es>
2017-05-25 15:45:10 +0200

    Implementing sensible initialization

--- src/jeco/core/algorithm/moge/AbstractProblemGE.java
+++ src/jeco/core/algorithm/moge/AbstractProblemGE.java
@@ -10,6 +10,7 @@ import jeco.core.util.bnf.Production;
 import jeco.core.util.bnf.Rule;
 import jeco.core.util.bnf.Symbol;
 import jeco.core.util.random.RandomGenerator;
+import org.apache.commons.math3.stat.StatUtils;
 
 public abstract class AbstractProblemGE extends Problem<Variable<Integer>> {
 
@@ -24,6 +25,8 @@ public abstract class AbstractProblemGE extends Problem<Variable<Integer>> {
 	protected int currentIdx;
 	protected int currentWrp;
 	protected boolean correctSol;
+        protected boolean sensibleInitialization;
+        protected double sensibleInitializationPercentile;
 
 	public AbstractProblemGE(String pathToBnf, int numberOfObjectives, int chromosomeLength, int maxCntWrappings, int codonUpperBound) {
 		super(chromosomeLength, numberOfObjectives);
@@ -35,6 +38,7 @@ public abstract class AbstractProblemGE extends Problem<Variable<Integer>> {
 			lowerBound[i] = 0;
 			upperBound[i] = codonUpperBound;
 		}
+                this.sensibleInitialization = false;
 	}
 
 	public AbstractProblemGE(String pathToBnf, int numberOfObjectives) {
@@ -44,6 +48,11 @@ public abstract class AbstractProblemGE extends Problem<Variable<Integer>> {
 	public AbstractProblemGE(String pathToBnf) {
 		this(pathToBnf, NUM_OF_OBJECTIVES_DEFAULT, CHROMOSOME_LENGTH_DEFAULT, MAX_CNT_WRAPPINGS_DEFAULT, CODON_UPPER_BOUND_DEFAULT);
 	}
+        
+        public void setSensibleInitialization(boolean value, double percentile) {
+            this.sensibleInitialization = value;
+            this.sensibleInitializationPercentile = percentile;
+        }
 
 	abstract public void evaluate(Solution<Variable<Integer>> solution, Phenotype phenotype);
 	
@@ -98,17 +107,60 @@ public abstract class AbstractProblemGE extends Problem<Variable<Integer>> {
 		}
 	}
 
+        @Override
 	public Solutions<Variable<Integer>> newRandomSetOfSolutions(int size) {
+            
+                int randomSize = size;
+                double[] consumedGenes = null;
+                int idx = 0;
+                
+                // In case of sensible initialization, half of the inidividuals are random.
+                if (sensibleInitialization) {
+                    randomSize = size / 2;
+                    // And store the consumed genes for each individual.
+                    consumedGenes = new double[randomSize];
+                }
+            
 		Solutions<Variable<Integer>> solutions = new Solutions<Variable<Integer>>();
-		for (int i=0; i<size; ++i) {
-			Solution<Variable<Integer>> solI = new Solution<Variable<Integer>>(numberOfObjectives);
-			for (int j = 0; j < numberOfVariables; ++j) {
-				Variable<Integer> varJ = new Variable<Integer>(RandomGenerator.nextInteger((int) upperBound[j])); 
-				solI.getVariables().add(varJ);
-			}
-			solutions.add(solI);
-		}
+                for (int i = 0; i < randomSize; ++i) {
+
+                    Solution<Variable<Integer>> solI = generateRandomSolution();
+                    solutions.add(solI);
+
+                    // Account for the number of genes that are consumed
+                    if (sensibleInitialization) {
+                        // Generate phenotype and make the count
+                        generatePhenotype(solI);
+                        consumedGenes[idx++] = (solI.getVariables().size() * currentWrp) + currentIdx;
+                    }
+                }
+                
+                // Complete the solutions creating long elements:
+                if (sensibleInitialization) {
+                    // The minimum size of the individuals depends on the previous.
+                    long minSize = Math.round(StatUtils.percentile(consumedGenes, sensibleInitializationPercentile));
+                    
+                    do {
+                        Solution<Variable<Integer>> solI = generateRandomSolution();
+                        generatePhenotype(solI);
+                        if (((solI.getVariables().size() * currentWrp) + currentIdx) >= minSize) {
+                            solutions.add(solI);
+                        }
+                    } while (solutions.size() < size);
+                    
+                }
+                
 		return solutions;
 	}
+
+        
+        private Solution<Variable<Integer>> generateRandomSolution() {
+            Solution<Variable<Integer>> solI = new Solution<>(numberOfObjectives);
+            for (int j = 0; j < numberOfVariables; ++j) {
+                Variable<Integer> varJ = new Variable<>(RandomGenerator.nextInteger((int) upperBound[j]));
+                solI.getVariables().add(varJ);
+            }
+            return solI;
+        }
 
 }
